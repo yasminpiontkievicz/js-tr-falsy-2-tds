5415e29397b9cbaaf5165fd557b3e1b1a9af0f8c
Yasmin Eduarda Piontkievicz <160152483+yasminpiontkievicz@users.noreply.github.com>
2024-11-22 12:05:08 -0300

    carga_js-tr-falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,34 @@
+const x = 0;
+//Boolean
+
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou falsy 
+//0 = fale 
+//1 = true
+
+console.log(0== false) // retorna true 
+console.log(""==false) // retorna true
+
+console.log(1==true)
+console.log("null e undefinned")
+//undefinned
+//null = vazio ou nada
+
+let minhaVar;
+let varNull=null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+console.log("quando o undefinned esta em sua forma pura")
+
+let numero = 3;
+let texto = "Alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
